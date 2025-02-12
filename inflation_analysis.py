20ec70c6af9da8ca8a1e6427ee23de6d434190e3
Roy Kisluk <roykisluk@mail.tau.ac.il>
2025-02-12 11:41:38 +0200

    Remove old indexing script and update binary files

--- inflation_analysis.py
+++ inflation_analysis.py
@@ -124,6 +124,141 @@ def grouping(start_year, end_year, cex_data_folder="/Users/roykisluk/Downloads/C
         'Druze': druze
     }, total_misparmb
 
+def tri_grouping(start_year, end_year, cex_data_folder="/Users/roykisluk/Downloads/Consumer_Expenditure_Survey/", 
+            young_age_cutoff=25, old_age_threshold=65, 
+            folder_names_pathname='Data_clean/CEX_folder_names.csv', 
+            age_groups_pathname='Data_clean/age_groups.csv'):
+    
+    import pandas as pd
+    import pyreadstat  
+
+    years = range(start_year, end_year + 1)
+
+    # Load folder names
+    folder_names_df = pd.read_csv(folder_names_pathname)
+
+    # Load age groups
+    age_groups_df = pd.read_csv(age_groups_pathname)
+    young_age_group_id = age_groups_df[(age_groups_df['min_age'] <= young_age_cutoff) & (age_groups_df['max_age'] >= young_age_cutoff)].index[0] + 1
+    old_age_group_id = age_groups_df[(age_groups_df['min_age'] <= old_age_threshold) & (age_groups_df['max_age'] >= old_age_threshold)].index[0] + 1
+
+    # Load household data for each year
+    dfs_HH = {}
+    for year in years:
+        subfolder = folder_names_df.loc[folder_names_df['Year'] == year, 'Folder_Name'].values[0]
+        data_HH_pathname = f"{cex_data_folder}{subfolder}/{subfolder}datamb.sas7bdat"
+        df, meta = pyreadstat.read_sas7bdat(data_HH_pathname)
+        df.columns = df.columns.str.lower()
+        if 'gil' in df.columns:
+            df.rename(columns={'gil': 'age_group'}, inplace=True)
+        dfs_HH[year] = df
+
+    # Load individual data for each year
+    dfs_IND = {}
+    for year in years:
+        subfolder = folder_names_df.loc[folder_names_df['Year'] == year, 'Folder_Name'].values[0]
+        data_IND_pathname = f"{cex_data_folder}{subfolder}/{subfolder}dataprat.sas7bdat"
+        df, meta = pyreadstat.read_sas7bdat(data_IND_pathname)
+        df.columns = df.columns.str.lower()
+        if 'gil' in df.columns:
+            df.rename(columns={'gil': 'age_group'}, inplace=True)
+        dfs_IND[year] = df
+
+    # Calculate the total number of misparmb for each year
+    total_misparmb = {}
+    for year in years:
+        total_misparmb[year] = dfs_HH[year]['misparmb'].nunique()
+
+    # Groups
+
+    # I: Demographic (Arabs, Haredi, Old, Young)
+    
+    # Arabs
+    arabs = {}
+    for year in years:
+        arabs[year] = dfs_HH[year][dfs_HH[year]['nationality'] == 2]
+
+    # Haredi
+    haredi = {}
+    for year in years:
+        if year >= 2014:  # From 2014 the variable 'RamatDatiyut' is available
+            haredi[year] = dfs_HH[year][dfs_HH[year]['ramatdatiyut'] == 4]
+        else:  # If the indicator is not available, we will use the lack of television and studies in yeshiva as a proxy
+            haredi[year] = dfs_HH[year][dfs_HH[year]['television'] == 0]
+            haredi[year] = haredi[year].merge(
+                dfs_IND[year][dfs_IND[year]['l_school'] == 10], 
+                on='misparmb', 
+                how='inner'
+            )
+
+    # Young
+    young = {}
+    for year in years:
+        young[year] = dfs_HH[year][dfs_HH[year]['misparmb'].isin(dfs_IND[year][(dfs_IND[year]['age_group'] <= young_age_group_id) & (dfs_IND[year]['y_kalkali'] == 1)]['misparmb'])]
+
+    # Old
+    old = {}
+    for year in years:
+        old[year] = dfs_HH[year][dfs_HH[year]['misparmb'].isin(dfs_IND[year][(dfs_IND[year]['age_group'] >= old_age_group_id) & (dfs_IND[year]['y_kalkali'] == 1)]['misparmb'])]
+
+    demographics = {
+        'Arabs': arabs,
+        'Haredi': haredi,
+        'Young': young,
+        'Old': old,
+    }
+
+    # II: Income (Deciles 1-10)
+    
+    # Income deciles
+    income = {}
+    for decile in range(1, 11):
+        income[decile] = {}  # Initialize the dictionary for each decile
+        for year in years:
+            income[decile][year] = dfs_HH[year][dfs_HH[year]['decile'] == decile]
+
+    # III: Socioeconomic status (1-5)
+
+    # SES (socioeconomic status) of locality
+    SES_locality = {}
+    for ses in range(1, 6):
+        SES_locality[ses] = {}
+        for year in years:
+            SES_locality[ses][year] = dfs_HH[year][dfs_HH[year]['cluster'] == ses]
+
+
+    return demographics, income, SES_locality, total_misparmb
+
+def get_n_obs(start_year, end_year, group_mmb=None, 
+            cex_data_folder = '/Users/roykisluk/Downloads/Consumer_Expenditure_Survey/', 
+            folder_names_pathname = 'Data_clean/CEX_folder_names.csv'):
+
+    import pandas as pd
+    import pyreadstat
+
+    # Load folder names
+    folder_names_df = pd.read_csv(folder_names_pathname)
+
+    years=range(start_year, end_year+1)
+    # Load price data for each year
+    dfs_prices = {}
+    for year in years:
+        subfolder = folder_names_df.loc[folder_names_df['Year'] == year, 'Folder_Name'].values[0]
+        data_prices_pathname = f"{cex_data_folder}{subfolder}/{subfolder}datayoman.sas7bdat"
+        df, meta = pyreadstat.read_sas7bdat(data_prices_pathname)
+        df.columns = df.columns.str.lower()
+        dfs_prices[year] = df
+
+    # Filter observations for relevant group
+    if group_mmb is not None:
+        for year in years:
+            dfs_prices[year] = dfs_prices[year][dfs_prices[year]['misparmb'].isin(group_mmb[year]['misparmb'])]
+
+    n_obs = {}
+    for year in years:
+        n_obs[year] = dfs_prices[year]['misparmb'].nunique()
+    
+    return n_obs
 
 def calculate_price_indexes(start_year, end_year, base_year, group_mmb = None, factor = 1, verbose = False,
                             cex_data_folder = '/Users/roykisluk/Downloads/Consumer_Expenditure_Survey/', 
@@ -131,7 +266,7 @@ def calculate_price_indexes(start_year, end_year, base_year, group_mmb = None, f
                             prodcode_dict_pathname = 'Data_clean/prodcode_dictionary_c3-c399.csv'):
     import pandas as pd
     import pyreadstat
-    import numpy as np  
+    import numpy as np 
     from tqdm import tqdm
 
     years = range(start_year, end_year + 1)
@@ -294,3 +429,146 @@ def calculate_price_indexes(start_year, end_year, base_year, group_mmb = None, f
     combined_primary_df = combined_primary_df.merge(prodcode_dict_df, on='prodcode', how='left')
 
     return combined_df, combined_secondary_df, combined_primary_df, yearly_price_index
+
+def output_data(groups, start_year, end_year, base_year=None, top_n=10, data_folder="/Users/roykisluk/Downloads/Consumer_Expenditure_Survey/"):
+    if base_year is None:   
+        base_year = start_year
+    years = range(start_year, end_year + 1)
+
+    groups_mmb = {key: {} for key in groups.keys()}
+    for key in groups:
+        for year in years:
+            groups_mmb[key][year] = groups[key][year][['misparmb']]
+
+    group_analysis = {}
+    for key in groups.keys():
+        group_number = list(groups.keys()).index(key) + 1
+        total_groups = len(groups)
+        print(f"Group {group_number}/{total_groups} ({key}) started.")
+        combined_df, combined_secondary_df, combined_primary_df, yearly_price_index = calculate_price_indexes(
+            start_year, end_year, base_year, group_mmb=groups_mmb[key], cex_data_folder=data_folder, verbose=False
+        )
+        group_analysis[key] = {
+            'combined_secondary_df': combined_secondary_df,
+            'combined_primary_df': combined_primary_df,
+            'yearly_price_index': yearly_price_index
+        }
+        print(f"Group {group_number}/{total_groups} ({key}) successfully computed.")
+    
+    return group_analysis, groups_mmb
+
+def output_obs_table(start_year, end_year, groups_mmb):
+    from tabulate import tabulate
+    import pandas as pd
+    total_observations_per_year = get_n_obs(start_year, end_year)
+    group_counts = {group: {year: len(groups_mmb[group][year]) for year in groups_mmb[group]} for group in groups_mmb}
+    # Create a dataframe with number of observations per year per group
+    observations_df = pd.DataFrame(group_counts).T
+    # Add total observations per year to the dataframe
+    observations_df.loc['Total'] = total_observations_per_year
+
+    # Calculate the relative share of each group per year
+    relative_share_df = observations_df.div(total_observations_per_year, axis=1) * 100
+
+    # Combine the absolute and relative values into a single dataframe
+    combined_df = observations_df.copy()
+    for col in observations_df.columns:
+        combined_df[col] = observations_df[col].astype(str) + " (" + relative_share_df[col].round(2).astype(str) + "%)"
+
+    # Display the dataframe
+    print(tabulate(combined_df, headers='keys', tablefmt='psql'))
+
+def price_index_over_time(group_analysis):
+    import matplotlib.pyplot as plt
+
+    # Extract yearly price indexes for each group
+    group_yearly_price_indexes = {group: group_analysis[group]['yearly_price_index'] for group in group_analysis}
+
+    # Plot the yearly price indexes
+    plt.figure(figsize=(12, 8))
+    for group, price_indexes in group_yearly_price_indexes.items():
+        years = list(price_indexes.keys())
+        indexes = list(price_indexes.values())
+        plt.plot(years, indexes, label=group)
+
+    plt.xlabel('Year')
+    plt.ylabel('Price Index')
+    plt.title('Yearly Price Index Comparison Between Groups')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def top_abs_weight_differences(weights_comparison_groups, weights_comparison_control, top_n=10):
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    n_groups = len(weights_comparison_groups)
+    nrows = (n_groups // 2) + (1 if n_groups % 2 != 0 else 0)
+    ncols = 2
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 15), sharey=False)
+    axes = axes.flatten()
+
+    weights_diff_df = {}
+    for group in weights_comparison_groups.keys():
+        # Calculate the weight differences between the group and the comparison group
+        weights_diff_df[group] = weights_comparison_groups[group].copy()
+        weights_diff_df[group]['weight_diff'] = weights_diff_df[group]['weight'] - weights_comparison_control['weight']
+
+        # Sort by the weight differences in descending order
+        sorted_weights_diff_df = weights_diff_df[group].sort_values(by='weight_diff', ascending=True)
+
+        # Select the top n positive gaps
+        top_n_positive = sorted_weights_diff_df.head(top_n)
+        top_n_positive = top_n_positive.iloc[::-1]
+
+        # Sort by the weight differences in ascending order
+        sorted_weights_diff_df = weights_diff_df[group].sort_values(by='weight_diff', ascending=False)
+
+        # Select the top n negative gaps
+        top_n_negative = sorted_weights_diff_df.head(top_n)
+
+        # Concatenate the positive and negative gaps
+        top_n_weights_diff_df = pd.concat([top_n_positive, top_n_negative])
+
+        # Plot the top n largest gaps
+        axes[list(weights_comparison_groups.keys()).index(group)].barh(top_n_weights_diff_df['description'], top_n_weights_diff_df['weight_diff'], color='skyblue')
+        axes[list(weights_comparison_groups.keys()).index(group)].set_title(group)
+        axes[list(weights_comparison_groups.keys()).index(group)].set_xlabel('Weight Difference')
+        axes[list(weights_comparison_groups.keys()).index(group)].set_ylabel('Description')
+        axes[list(weights_comparison_groups.keys()).index(group)].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+def top_price_increase_contributors(group_analysis, top_n=10):
+    import matplotlib.pyplot as plt
+
+    fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(20, 15), sharey=False)
+    axes = axes.flatten()
+
+    for i, (group, analysis) in enumerate(group_analysis.items()):
+        secondary_df = analysis['combined_secondary_df']
+        # Filter for increases in price indexes
+        increased_price_df = secondary_df[secondary_df['price_index'] > 100]
+        # Calculate the contribution to the yearly price index
+        increased_price_df = increased_price_df.copy()  # Ensure you're working with a copy
+        increased_price_df.loc[:, 'contribution'] = increased_price_df['price_index'] * increased_price_df['weight']    
+        # Sort by contribution in descending order
+        top_contributors = increased_price_df.sort_values(by='contribution', ascending=False).head(top_n)
+        
+        # Replace NaN values in 'description' with a placeholder text
+        top_contributors['description'] = top_contributors['description'].fillna('No Description')
+        
+        # Plot the top contributors
+        axes[i].barh(top_contributors['description'], top_contributors['contribution'], color='skyblue')
+        axes[i].set_title(group)
+        axes[i].set_xlabel('Contribution to Price Index')
+        axes[i].set_ylabel('Description')
+        axes[i].grid(True)
+
+    # Remove any empty subplots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
+    plt.show()
